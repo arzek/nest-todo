cb4d7a1ebdf2851dc4616344c0dfa1b94378739a
Artem Bondarchuk <artem.b@zazmic.com>
2019-08-26 15:05:07 +0300

    get all todo and g documentation

--- src/todos/create-todo.dto.ts
+++ src/todos/create-todo.dto.ts
@@ -1,6 +1,8 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class CreateTodoDto {
   @IsNotEmpty()
+  @ApiModelProperty()
   readonly name: string;
 }
--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,11 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './create-todo.dto';
 import { TodoEntity } from './todo.entity';
+import { ApiUseTags } from '@nestjs/swagger';
 
+@ApiUseTags('Todos')
 @Controller('todos')
 export class TodosController {
   constructor(private readonly todoService: TodosService) {}
+
+  @Get()
+  findAll(): Promise<TodoEntity[]> {
+    return this.todoService.findAll();
+  }
 
   @Post()
   create(@Body() createTodoDto: CreateTodoDto): Promise<TodoEntity> {
